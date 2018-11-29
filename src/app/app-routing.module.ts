de42de3e294aadbba2bd7b6c2c0bdf1efb5f0f6d
Piero Bravo <pbravogutarra@outlook.com>
2018-11-29 15:02:08 -0500

    agregar routin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {RegistroTareoComponent} from './components/registroTareo/registroTareo.component'
+import {AsignacionResponsableComponent} from './components/asignacionResponsable/asignacionResponsable.component'
+import {AsignacionGrupoComponent} from './components/asignacionGrupo/asignacionGrupo.componente'
+import {MenuComponent} from './components/shared/menu/menu.component'
+
 
 const routes: Routes = [
+  { path: 'menu', component: MenuComponent},
+  { path: 'asignacionGrupo', component: AsignacionGrupoComponent},
+  { path: 'asignacionResponsable', component: AsignacionResponsableComponent},
   { path: 'registroTareo', component: RegistroTareoComponent},
-  { path: '**' , pathMatch: 'full' ,redirectTo: 'login'}
+  { path: '**' , pathMatch: 'full' ,redirectTo: 'menu'}
 ];
 
 @NgModule({
