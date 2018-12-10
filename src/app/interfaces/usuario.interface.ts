49f7a49f9331acd8f55ae82b0f046b1d7f93c9ad
dcamusa <45017346+dcamusa@users.noreply.github.com>
2018-12-10 15:43:40 -0500

    avance de reporte

--- src/app/interfaces/usuario.interface.ts
+++ src/app/interfaces/usuario.interface.ts
@@ -3,6 +3,7 @@ export interface Usuario{
     Numero_documento: string;
     NombreCompleto: string;
     Responsable: number;
+    codigo_usuario: number;
     Exito?: boolean;
     Mensaje: string;
     Token?: string;
