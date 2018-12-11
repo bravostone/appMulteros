aed72e31c87b09e6099a0e4d8b869777f89c7b52
Alexi Espinoza C <alexi_system@outlook.com>
2018-12-11 17:36:14 -0500

    cambio de reporte individual

--- src/app/components/reporte-indivdual/reporte-indivdual.component.ts
+++ src/app/components/reporte-indivdual/reporte-indivdual.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReporteIndividualRequest } from "../../interfaces/reporteIndividual.interface";
+import { ReporteIndividualResponse } from "../../interfaces/reporteIndividual.interface";
+import { ReporteIndividualService } from '../../services/reporteIndividual.service';
 
 @Component({
   selector: 'app-reporte-indivdual',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporte-indivdual.component.css']
 })
 export class ReporteIndivdualComponent implements OnInit {
-
-  constructor() { }
+  listaDatos : ReporteIndividualResponse;
+  request : ReporteIndividualRequest;
+  constructor(private _reporte : ReporteIndividualService) { }
 
   ngOnInit() {
+    this.getDatos();
+  }
+
+  getDatos(){
+    this.request = {
+      YearActual: 2018, 
+      SemanaActual: 0,
+      MesActual: 12
+    };
+
+    this._reporte.getReporteIndividual(this.request).subscribe(
+      (data: any) =>{
+          this.listaDatos = data
+      }
+    );
+
   }
 
 }
