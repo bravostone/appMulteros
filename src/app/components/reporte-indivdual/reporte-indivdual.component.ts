2097939471b02f4e525e2be8cea06eedf5211202
DBAOscar <adrian_ortiz92>
2018-12-20 09:55:17 -0500

    Arreglando ichus

--- src/app/components/reporte-indivdual/reporte-indivdual.component.ts
+++ src/app/components/reporte-indivdual/reporte-indivdual.component.ts
@@ -1,47 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ReporteIndividualRequest } from "../../interfaces/reporteIndividual.interface";
 import { ReporteIndividualResponse } from "../../interfaces/reporteIndividual.interface";
 import { ReporteIndividualSelect } from "../../interfaces/reporteIndividual.interface";
-import { ReporteIndividualService } from '../../services/reporteIndividual.service';
-import { LoadingBarService } from '@ngx-loading-bar/core';
+import { ReporteIndividualService } from "../../services/reporteIndividual.service";
+import { LoadingBarService } from "@ngx-loading-bar/core";
 
-import { Chart } from 'angular-highcharts';
+import { Chart } from "angular-highcharts";
 
 @Component({
-  selector: 'app-reporte-indivdual',
-  templateUrl: './reporte-indivdual.component.html',
-  styleUrls: ['./reporte-indivdual.component.css']  
+  selector: "app-reporte-indivdual",
+  templateUrl: "./reporte-indivdual.component.html",
+  styleUrls: ["./reporte-indivdual.component.css"]
 })
 export class ReporteIndivdualComponent implements OnInit {
   chart = null;
   public ListaPuntos: number[] = [];
   public ListaNombres: string[] = [];
-  listaDatos : ReporteIndividualResponse[];
-  mes :string = "0" ; 
-  anio :string= "0" ;   
-  request : ReporteIndividualRequest;
-  public ListaAnios: number[] = [2018,2019];
-  public ListaMeses: ReporteIndividualSelect[] = [{codigo:1,nombre:"Enero"},
-    {codigo:2,nombre:"Febrero"},
-    {codigo:3,nombre:"Marzo"},
-    {codigo:4,nombre:"Abril"},
-    {codigo:5,nombre:"Mayo"},
-    {codigo:6,nombre:"Junio"},
-    {codigo:7,nombre:"Julio"},
-    {codigo:8,nombre:"Agosto"},
-    {codigo:9,nombre:"Septiembre"},
-    {codigo:10,nombre:"Octubre"},
-    {codigo:11,nombre:"Noviembre"},
-    {codigo:12,nombre:"Diciembre"}
+  listaDatos: ReporteIndividualResponse[];
+  mes: number = new Date().getMonth() + 1;
+  anio: number = new Date().getFullYear();
+  request: ReporteIndividualRequest;
+  public ListaAnios: number[] = [2018, 2019];
+  public ListaMeses: ReporteIndividualSelect[] = [
+    { codigo: 1, nombre: "Enero" },
+    { codigo: 2, nombre: "Febrero" },
+    { codigo: 3, nombre: "Marzo" },
+    { codigo: 4, nombre: "Abril" },
+    { codigo: 5, nombre: "Mayo" },
+    { codigo: 6, nombre: "Junio" },
+    { codigo: 7, nombre: "Julio" },
+    { codigo: 8, nombre: "Agosto" },
+    { codigo: 9, nombre: "Septiembre" },
+    { codigo: 10, nombre: "Octubre" },
+    { codigo: 11, nombre: "Noviembre" },
+    { codigo: 12, nombre: "Diciembre" }
   ];
-  constructor(private _reporte : ReporteIndividualService,private loadingBar: LoadingBarService) { }
+  constructor(
+    private _reporte: ReporteIndividualService,
+    private loadingBar: LoadingBarService
+  ) {}
 
   ngOnInit() {
     //this.getDatos();
     // setTimeout(() => {
     //   this.setArmarArrays();
     // }, 2000);
-    
     // setTimeout(() => {
     //   this.chart = new Chart({
     //     chart: {
@@ -54,16 +57,16 @@ export class ReporteIndivdualComponent implements OnInit {
     //       enabled: false
     //     },
     //     xAxis: {
-    //       categories: this.ListaNombres                    
+    //       categories: this.ListaNombres
     //     },
-    //     yAxis: { 
+    //     yAxis: {
     //       //tickInterval: 1,
     //       allowDecimals: false,
     //       title: {
     //         text: '', //values
     //         align: 'high'
-    //     } 
-    //      }, 
+    //     }
+    //      },
     //     series: [
     //       {
     //         name: 'total Puntos',
@@ -73,7 +76,7 @@ export class ReporteIndivdualComponent implements OnInit {
     //       }
     //     ]
     //   });
-    // }, 2000);    
+    // }, 2000);
   }
   startLoading() {
     this.loadingBar.start();
@@ -81,77 +84,74 @@ export class ReporteIndivdualComponent implements OnInit {
   stopLoading() {
     this.loadingBar.complete();
   }
-  getDatos(){
+  getDatos() {
     // this.request = {
-    //   YearActual: 2018, 
+    //   YearActual: 2018,
     //   SemanaActual: 0,
     //   MesActual: 12
     // };
 
-    this._reporte.getReporteIndividual(this.request).subscribe(
-      (data: any) =>{
-          //console.log(data);
-          this.listaDatos = data
-          //console.log(this.listaDatos);
-      }
-    );    
+    this._reporte.getReporteIndividual(this.request).subscribe((data: any) => {
+      //console.log(data);
+      this.listaDatos = data;
+      //console.log(this.listaDatos);
+    });
   }
-  getReporte(anio:number,mes:number){
+  getReporte(anio: number, mes: number) {
     this.startLoading();
     this.request = {
-      YearActual: anio, 
+      YearActual: anio,
       SemanaActual: 0,
       MesActual: mes
     };
     console.log(this.request);
 
     this.getDatos();
-     setTimeout(() => {
+    setTimeout(() => {
       this.setArmarArrays();
     }, 2000);
 
-     setTimeout(() => {
+    setTimeout(() => {
       this.chart = new Chart({
         chart: {
-          type: 'bar'
+          type: "bar"
         },
         title: {
-          text: 'Puntos Individuales del Mes'
+          text: "Puntos Individuales del Mes"
         },
         credits: {
           enabled: false
         },
         xAxis: {
-          categories: this.ListaNombres                    
+          categories: this.ListaNombres
         },
-        yAxis: { 
+        yAxis: {
           //tickInterval: 1,
           allowDecimals: false,
           title: {
-            text: '', //values
-            align: 'high'
-        } 
-         }, 
+            text: "", //values
+            align: "high"
+          }
+        },
         series: [
           {
-            name: 'total Puntos',
+            name: "total Puntos",
             colorIndex: 6,
-            pointWidth:20,
+            pointWidth: 20,
             data: this.ListaPuntos
           }
         ]
       });
       this.stopLoading();
     }, 2000);
-        
   }
-  setArmarArrays(){
+  setArmarArrays() {
     console.log("pintar lista datos asignados");
     console.log(this.listaDatos);
-    let i:Number = 0;
+    let i: Number = 0;
     this.ListaNombres = [];
     this.ListaPuntos = [];
-    for(let item of this.listaDatos){
+    for (let item of this.listaDatos) {
       this.ListaNombres.push(item.NombreCompleto);
       this.ListaPuntos.push(parseInt(item.puntos_mes));
     }
@@ -160,5 +160,4 @@ export class ReporteIndivdualComponent implements OnInit {
   add() {
     this.chart.addPoint(Math.floor(Math.random() * 10));
   }
-
 }
